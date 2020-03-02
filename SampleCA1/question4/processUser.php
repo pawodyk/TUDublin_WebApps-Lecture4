9c6e5b88a50b7bba1f66df2eb97e935eab593baf
Pawel <14218944+pwodyk@users.noreply.github.com>
2020-03-02 11:31:37 +0000

    completed Q4C

--- SampleCA1/question4/processUser.php
+++ SampleCA1/question4/processUser.php
@@ -2,19 +2,46 @@
 
 namespace Tudublin;
 
+$isValidated = true;
+$output = '';
+$errorList = [];
+
 $userName = filter_input(INPUT_GET, 'name');
 $userAge = filter_input(INPUT_GET, 'age');
 $isLect = filter_input(INPUT_GET, 'isLecturer');
 
-$output = "Data Recived:<br><br>";
-
-$output .= "Name: $userName <br>";
-$output .= "Age: $userAge <br>";
-if (is_null($isLect)){
-    $output .= "User Type: (not known)";
-} else {
-    $output .= "User Type: lecturer";
+if (empty($userName)){
+    $isValidated = false;
+    $errorList[] = "name field is missing";
 }
+if (empty($userAge)){
+    $isValidated = false;
+    $errorList[] = "age field is missing";
+} elseif (!is_numeric($userAge)){
+    $isValidated = false;
+    $errorList[] = "age is not a number";
+}
+
+if ($isValidated){
+    $output = 'Data Recived:<br><br>';
+
+
+    $output .= "Name: $userName <br>";
+    $output .= "Age: $userAge <br>";
+    if (is_null($isLect)){
+        $output .= "User Type: (not known)";
+    } else {
+        $output .= "User Type: lecturer";
+    }
+} else {
+    $output = 'Data received - errors<br><br>';
+    foreach ($errorList as $err){
+        $output .= " - $err <br>";
+    }
+}
+
+
+
 
 print $output;
 
