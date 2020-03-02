54329f332251e5029c32ead64f07669c7acce344
Pawel <14218944+pwodyk@users.noreply.github.com>
2020-03-02 09:37:16 +0000

    completed Q2A

---
+++ SampleCA1/question2/templates/about.php
@@ -0,0 +1,13 @@
+<!DOCTYPE>
+<html>
+<head>
+    <title>About Page</title>
+
+</head>
+<body>
+
+    <h1>About page</h1>
+    <p>back to <a href="index.php">home</a><p>
+
+</body>
+</html>
---
+++ SampleCA1/question2/templates/home.php
@@ -0,0 +1,17 @@
+<!DOCTYPE>
+<html>
+<head>
+    <title>Home Page</title>
+
+</head>
+<body>
+
+    <h1>Home page</h1>
+    <ul>
+        <li>
+            <a href="index.php?action=about">about page</a>
+        </li>
+    </ul>
+
+</body>
+</html>
