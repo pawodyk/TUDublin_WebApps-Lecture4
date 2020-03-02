fe35c79841a820c107b3c54b3b236a6c55c86fd5
Pawel <14218944+pwodyk@users.noreply.github.com>
2020-03-02 09:20:55 +0000

    initial commit for sample test

---
+++ SampleCA1/index.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html>
+<head><title>test form</title></head>
+<body>
+
+<h1>Campus user</h1>
+<form action="processUser.php" method="GET">
+    Name:
+    <input name="name">
+
+    <p>
+        Age:
+        <input name="age">
+
+    <p>
+        Is lecturer:
+        <input type="checkbox" name="isLecturer">
+    </p>
+
+    <p>
+        <input type="submit">
+</form>
+
+</body>
+</html>
