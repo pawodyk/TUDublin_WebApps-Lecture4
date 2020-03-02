1ab82ef27f8e8b9b9afb74acef9987f7d04dc954
Pawel <14218944+pwodyk@users.noreply.github.com>
2020-03-02 10:24:17 +0000

    completed Q3A

---
+++ SampleCA1/question3/src/MainController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tudublin;
+
+class MainController {
+
+    const TEMPLATES_PATH = __DIR__ . '/../templates';
+    private $twig;
+
+    public function __construct(){
+        $loader =  new \Twig\Loader\FilesystemLoader(self::TEMPLATES_PATH);
+        $this->twig = new \Twig\Environment($loader);
+    }
+
+    private function renderPage($name, $args){
+        $template = $this->twig->render($name, $args);
+        print($template);
+    }
+
+    public function homepage(){
+        $template = 'home.html.twig';
+        $args = ['pageTitle'=> 'Home Page'];
+        $this->renderPage($template,$args);
+    }
+
+    public function about(){
+        $template = 'about.html.twig';
+        $args = ['pageTitle'=>"About Page"];
+        $this->renderPage($template,$args);
+    }
+
+
+}
